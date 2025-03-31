3604cfe555f5ded1cb937ff489b7559aeb7fdf24
Pavel Tatarintsev <ic472pbt@ya.ru>
2025-03-31 20:39:28 +0400

    create IO module for io operations

---
+++ IO.fs
@@ -0,0 +1,31 @@
+﻿module IO
+    open System.IO
+    open FSharp.Control
+    let readBlockSize = config.block_size * 512
+    printfn "read block: %i" readBlockSize
+    
+    let fileSize = 
+        use f = File.Open(config.input_file, FileMode.Open, FileAccess.Read)
+        f.Length
+    printfn "file size: %i" fileSize
+    let blocks = fileSize / (int64)readBlockSize
+    printfn "blocks: %i" blocks
+    
+    let sw = System.Diagnostics.Stopwatch.StartNew()
+    let readBlock = asyncSeq{
+        let mutable position = 0L
+        let mutable len = 0
+        let readBuffer = Array.zeroCreate<byte> readBlockSize
+        use f = File.Open(config.input_file, FileMode.Open, FileAccess.Read)
+        let! length = f.ReadAsync(readBuffer, 0, readBuffer.Length) |> Async.AwaitTask
+        len <- length
+        while len > 0 && len = readBlockSize do
+            yield readBuffer, position
+            let currentBlock = position / (int64) readBlockSize
+            printfn "position block: %i/%i (%.01f%%) speed: %.01fMb/s" 
+                currentBlock blocks ((float)currentBlock/(float)blocks*100.0)
+                ((float) position / 1024.0 / 1024.0 / sw.Elapsed.TotalSeconds)
+            position <- position + int64 length
+            let! length = f.ReadAsync(readBuffer, 0, readBuffer.Length) |> Async.AwaitTask
+            len <- length
+    }
--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿open System
 open System.IO
 open Zip
+open IO
 
 open FSharp.Control
 open Brahma.FSharp
@@ -24,40 +25,6 @@ let wordsMax = config.words_max
 
 let workGroupSize = config.work_size
 printfn "work group size: %i" workGroupSize
-let readBlockSize = config.block_size * 512
-printfn "read block: %i" readBlockSize
-
-
-
-
-
-let fileSize = 
-    use f = File.Open(config.input_file, FileMode.Open, FileAccess.Read)
-    f.Length
-printfn "file size: %i" fileSize
-let blocks = fileSize / (int64)readBlockSize
-printfn "blocks: %i" blocks
-
-
-let sw = System.Diagnostics.Stopwatch.StartNew()
-
-let readBlock = asyncSeq{
-    let mutable position = 0L
-    let mutable len = 0
-    let readBuffer = Array.zeroCreate<byte> readBlockSize
-    use f = File.Open(config.input_file, FileMode.Open, FileAccess.Read)
-    let! length = f.ReadAsync(readBuffer, 0, readBuffer.Length) |> Async.AwaitTask
-    len <- length
-    while len > 0 && len = readBlockSize do
-        yield readBuffer, position
-        let currentBlock = position / (int64) readBlockSize
-        printfn "position block: %i/%i (%.01f%%) speed: %.01fMb/s" 
-            currentBlock blocks ((float)currentBlock/(float)blocks*100.0)
-            ((float) position / 1024.0 / 1024.0 / sw.Elapsed.TotalSeconds)
-        position <- position + int64 length
-        let! length = f.ReadAsync(readBuffer, 0, readBuffer.Length) |> Async.AwaitTask
-        len <- length
-}
 
 let degreeOfParallelizm = System.Environment.ProcessorCount
 
